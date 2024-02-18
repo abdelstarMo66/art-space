fcec53c71420c3866754ca87a05dc52e17c6e42a
Mohamed Abdelstar <112125899+abdelstarMo66@users.noreply.github.com>
2024-02-18 19:41:08 +0200

    cartCycle

--- src/validations/cartValidation.js
+++ src/validations/cartValidation.js
@@ -2,7 +2,33 @@ const {param, body} = require("express-validator");
 
 const ApiError = require("../utils/apiError");
 const CartModel = require("../models/cartModel");
+const ProductModel = require("../models/productModel");
 
-exports.addProductToCartValidation = []
+exports.addProductToCartValidation = [
+    body("productId")
+        .notEmpty()
+        .withMessage("Product is required")
+        .isMongoId()
+        .withMessage("product id not valid")
+        .custom((productId) =>
+            ProductModel.findById(productId).then((product) => {
+                if (!product) {
+                    return Promise.reject(new ApiError(`No product for this id: ${product}`, 404));
+                }
+            })
+        ),
+]
 
-exports.deleteSpecificProductFromCartValidation = []
+exports.deleteSpecificProductFromCartValidation = [
+    param("itemId")
+        .isMongoId()
+        .withMessage("Invalid Cart Id")
+        .custom(async (val) => {
+            const category = await CartModel.findById(val);
+
+            if (!category) {
+                return Promise.reject(new ApiError(`no cart for this id ${val}`, 404));
+            }
+            return true;
+        }),
+]
