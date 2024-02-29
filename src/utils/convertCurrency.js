b153c0e3aa5616a4a093f3c731388e8875cdc7da
Mohamed Abdelstar <112125899+abdelstarMo66@users.noreply.github.com>
2024-02-29 16:18:08 +0200

    fixIssue

--- src/utils/convertCurrency.js
+++ src/utils/convertCurrency.js
@@ -3,7 +3,7 @@ const CurrencyConverter = require("currency-converter-lt")
 const convertCurrency = async (from, to, amount) => {
     let currencyConverter = new CurrencyConverter()
 
-    return parseInt(await currencyConverter.from(from).to(to).amount(Number(amount)).convert());
+    return await currencyConverter.from(from).to(to).amount(Number(amount)).convert();
 }
 
 module.exports = convertCurrency
