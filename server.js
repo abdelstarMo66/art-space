f5522c69c5e6c9bc095c820dd03ad00291179616
abdelstarMo66 <112125899+abdelstarMo66@users.noreply.github.com>
2024-03-14 13:27:48 +0200

    update

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ const server = app.listen(PORT, "localhost", () => {
     console.log(`App running on port ${PORT}`);
 });
 
-export default app;
+module.exports = server
 
 process.on("unhandledRejection", (error) => {
     console.error(`unhandledRejection Error: ${error.name} | ${error.message}`);
