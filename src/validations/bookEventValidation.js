04e805675d614035b3183be135bb2f1d596fcfe0
abdelstarMo66 <112125899+abdelstarMo66@users.noreply.github.com>
2024-05-01 21:01:23 +0300

    validation edit

--- src/validations/bookEventValidation.js
+++ src/validations/bookEventValidation.js
@@ -2,17 +2,25 @@ const {param} = require("express-validator");
 
 const ApiError = require("../utils/apiError");
 const EventModel = require("../models/eventModel");
+const BookEventModel = require("../models/bookEventModel");
 
 exports.bookEventValidation = [
     param("eventId")
         .isMongoId()
         .withMessage("invalid event id")
-        .custom(async (val) => {
+        .custom(async (val, {req}) => {
             const event = await EventModel.findById(val);
 
             if (!event) {
                 return Promise.reject(new ApiError(`no event found for this id ${val}`, 404));
             }
+
+            const check = await BookEventModel.find({user: req.loggedUser._id, events: {$in: [val]}});
+
+            if (check) {
+                return Promise.reject(new ApiError(`this event already booked`, 400));
+            }
+
             return true;
         }),
 ]
--- src/validations/registerAuctionValidation.js
+++ src/validations/registerAuctionValidation.js
@@ -2,17 +2,28 @@ const {param} = require("express-validator");
 
 const ApiError = require("../utils/apiError");
 const AuctionModel = require("../models/auctionModel");
+const RegisterAuctionModel = require("../models/registerAuctionModel");
 
 exports.registerToAuctionValidation = [
     param("auctionId")
         .isMongoId()
         .withMessage("invalid auction id")
-        .custom(async (val) => {
+        .custom(async (val, {req}) => {
             const product = await AuctionModel.findById(val);
 
             if (!product) {
                 return Promise.reject(new ApiError(`no product found for this id ${val}`, 404));
             }
+
+            const check = await RegisterAuctionModel.find({
+                user: req.loggedUser._id,
+                auction: {$in: [{auctionId: val}]}
+            });
+
+            if (check) {
+                return Promise.reject(new ApiError(`you are already registered in this auction`, 400));
+            }
+
             return true;
         }),
 ]
